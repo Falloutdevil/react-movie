47af99098bb58808e5ac28c036e61e2dbd5e279e
Falloutdevil <Falloutdevil@ya.ru>
2022-02-06 21:07:46 +0300

    add card movie and fetch

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,33 @@
-function Main() {
-    return (
-        <main className='container content'>
-            Hello from Movies App
-        </main>
-    )
+import React from 'react';
+import {Movies} from '../components/Movies';
+
+class Main extends React.Component{
+    state = {
+        movies: [],
+    }
+
+    componentDidMount() {
+        fetch('https://www.omdbapi.com/?i=tt3896198&apikey=8ec9e60d&s=avengers')
+            .then(response => response.json())
+            .then(data => this.setState({movies: data.Search}))
+    }
+
+    render() {
+        const {movies} = this.state;
+
+        return (
+            <main className='container content'>
+                {
+                    movies.length ? (
+                    <Movies movies={this.state.movies}/>
+                    ) : <h3>Loading...</h3>
+                }
+
+            </main>
+        )
+    }
+
+
 }
 
 export {Main}
