e99a8fcffb9fd88583f6579c948848d8c8f54948
Falloutdevil <Falloutdevil@ya.ru>
2022-02-07 19:49:11 +0300

    add api_key

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,6 +3,8 @@ import {Movies} from '../components/Movies';
 import {Preloader} from '../components/Preloader';
 import {Search} from '../components/Search';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 class Main extends React.Component {
     state = {
         movies: [],
@@ -10,14 +12,17 @@ class Main extends React.Component {
     }
 
     componentDidMount() {
-        fetch('https://www.omdbapi.com/?i=tt3896198&apikey=8ec9e60d&s=avengers')
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
             .then(response => response.json())
             .then(data => this.setState({movies: data.Search, loading: false}))
     }
 
     searchMovies = (str, type = 'all') => {
-        this.setState({loading: true})
-        fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=8ec9e60d&s=${str}${type !== 'all' ? `&type=${type}` : ''}`
+        this.setState({ loading: true });
+        fetch(
+            `https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
+                type !== 'all' ? `&type=${type}` : ''
+            }`
         )
             .then(response => response.json())
             .then(data => this.setState({movies: data.Search, loading: false}))
@@ -29,12 +34,10 @@ class Main extends React.Component {
         return (
             <main className="container content">
                 <Search searchMovies={this.searchMovies}/>
-                {loading ? <Preloader/> : <Movies movies={this.state.movies}/>}
+                {loading ? <Preloader/> : <Movies movies={movies}/>}
             </main>
-        )
+        );
     }
-
-
 }
 
 export {Main}
